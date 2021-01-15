18a56e3a34092f8582d42636da8722adf680efae
Kseniya <stashchenkoksu@mail.ru>
2021-01-15 12:45:54 +0300

    refactor project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,14 +1,6 @@
 class ProjectsController < ApplicationController
   load_and_authorize_resource
 
-  #def index
-  #@projects = Project.all
-  #end
-
-  #def new
-  #@project = Project.new
-    #end
-
   def create
     @project = Project.new(project_params)
 
@@ -19,22 +11,13 @@ class ProjectsController < ApplicationController
     end
   end
 
-  #def edit
-  #@project = Project.find(params[:id])
-  # end
-
   def update
-    #@project = Project.find(params[:id])
     if @project.update(project_params)
       redirect_to @project
     else
       render :edit
     end
   end
-
-  #def show
-  #@project = Project.find(params[:id])
-  #end
 
   def destroy
     @project = Project.find(params[:id])
