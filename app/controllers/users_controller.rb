35cf9b4c658dd265ea0fe58a3a54dcb102e8221f
Kseniya <stashchenkoksu@mail.ru>
2021-02-14 18:22:37 +0300

    nothing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
     @tasks = @user.tasks.where(["title LIKE?", "%#{params[:search]}%"]).paginate(page: params[:page], per_page: 5)
-    @tasks = TaskFilter.call(tasks: @tasks, params: params, user: current_user)
     @projects = []
     @tasks.each{|task| @projects << task.project_id}
     @projects.uniq!
